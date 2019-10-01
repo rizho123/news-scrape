fc4de456df02967897ad2a019fe091c1e8a4010b
rizho123 <v-riczho@microsoft.com>
2019-10-01 02:53:39 -0700

    updating

--- controllers/scrape-controller.js
+++ controllers/scrape-controller.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var db = require("../models");
 
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://heroku_8l6pnsrr:js72uqfi8v4ji0hffu63n9u4kr@ds229108.mlab.com:29108/heroku_8l6pnsrr", {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true
 })
 
