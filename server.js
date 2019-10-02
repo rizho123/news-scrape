516a846f5f83fbf6bc36844b84b0ee7c88f74284
rizho123 <richardzhou123@gmail.com>
2019-10-02 03:29:26 -0700

    updating

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-//===== Dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 
-//===== App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//===== Load Routes
 const index = require('./routes/index');
 const api = require('./routes/api');
 
@@ -18,20 +15,16 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/news-s
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-//===== Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-//===== Handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-//===== Use Routes
 app.use('/', index);
 app.use('/api', api);
 
-//===== Listen
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
